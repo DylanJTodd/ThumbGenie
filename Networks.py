44ae5acbf1db8a391d1c60f11ee9e7cd4a054b97
DylanJTodd <dylan.todd04@gmail.com>
2024-05-29 22:00:07 -0400

    Added Networks file for improved readability

---
+++ Networks.py
@@ -0,0 +1,58 @@
+import numpy
+import torch
+from torch import nn
+from torch.nn import functional
+
+
+                        ###############################################
+                        ### Convert a batch of title and categories ###
+                        ###       into dense, embedded vectors      ###
+                        ###############################################
+
+class CategoryTitleEmbeddingNet(nn.Module):
+    def __init__(self, num_categories:int, category_embedding_dim:int, vocab_size:int, title_embedding_dim:int, title_max_length:int):
+        """
+        Initializes the title / category embedding neural network
+
+        Args:
+            num_categories:          Total number of unique categories.
+            category_embedding_dim:  Dimensions of the category embedding vector.
+            vocab_size:              Total number of unique tokens.
+            title_embedding_dim:     Dimensions of the title embedding vector.
+            title_max_length:        Maximum number of tokens allowed in the title.
+
+        Returns:
+            None
+        """
+        super(CategoryTitleEmbeddingNet, self).__init__()
+        self.category_embedding = nn.Embedding(num_categories, category_embedding_dim)
+        self.title_embedding = nn.Embedding(vocab_size, title_embedding_dim)
+
+        self.fully_connected1 = nn.Linear(category_embedding_dim + title_embedding_dim * title_max_length, 256)
+        self.fully_connected2 = nn.Linear(256, 128)
+        self.fully_connected3 = nn.Linear(128, 64)
+        self.fully_connected4 = nn.Linear(64, 1)
+        
+    def forward(self, category_indices:torch.Tensor, title_indices:torch.Tensor) -> torch.Tensor:
+        """
+        
+
+        Args:
+            category_indices:       A tensor of unique numerical indices that represent the category
+            title_indices:          A tensor of unique numerical indices that represent vocab words in the title.
+
+        Returns:
+            final_output:           A dense vector representing the title and category
+        """
+
+        category_embedded = self.category_embedding(category_indices)
+        title_embedded = self.title_embedding(title_indices)
+        title_embedded = title_embedded.view(title_embedded.size(0), -1)
+        combined_embeddings = torch.cat((category_embedded, title_embedded), dim=1)
+
+        combined_output = torch.relu(self.fully_connected1(combined_embeddings))
+        combined_output = torch.relu(self.fully_connected2(combined_output))
+        combined_output = torch.relu(self.fully_connected3(combined_output))
+        final_output = self.fully_connected4(combined_output)
+        return final_output
+    
